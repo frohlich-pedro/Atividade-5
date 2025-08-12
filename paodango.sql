579392e50783fcc659633cef7334362fbc81e21a
Carlos Bittencourt <carlos_bittencourt2@estudante.sesisenai.org.br>
2025-08-12 08:15:53 -0300

    UPDATE - tabela 'cliente' criada

--- paodango.sql
+++ paodango.sql
@@ -2,9 +2,11 @@ CREATE DATABASE paodango;
 
 USE paodango;
 
-CREATE TABLE usuarios (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
-    email VARCHAR(150) NOT NULL UNIQUE,
+CREATE TABLE cliente (
+    id_cliente INT AUTO_INCREMENT PRIMARY KEY,
+    nome_cliente VARCHAR(100) NOT NULL,
+    email_cliente VARCHAR(150) NOT NULL UNIQUE,
+    telefone_cliente VARCHAR(15) NOT NULL,
+    cpf_cliente VARCHAR(11) NOT NULL UNIQUE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
