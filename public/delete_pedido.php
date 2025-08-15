b40fccf3280d734c0fd7c8fb8345e6ffc6333906
Marco-Tomasini <marco_tomasini@estudante.sesisenai.org.br>
2025-08-15 12:24:29 -0300

    UPDATE - delete pedido atualizado

--- public/delete_pedido.php
+++ public/delete_pedido.php
@@ -2,7 +2,7 @@
 include 'db.php';
 
 if (!isset($_GET['id_pedido']) || !is_numeric($_GET['id_pedido'])) {
-    echo "ID inválido. <a href='../index.php'>Voltar</a>";
+    echo "ID inválido. <a href='tabela_pedido.php'>Voltar</a>";
     exit;
 }
 
@@ -12,7 +12,7 @@ $sql = "DELETE FROM pedido WHERE id_pedido = $id_pedido";
 
 if ($conn->query($sql) === true) {
     echo "Pedido excluído com sucesso.
-        <a href='../index.php'>Ver registros.</a>";
+        <a href='tabela_pedido.php'>Ver registros.</a>";
 } else {
     echo "Erro: " . $conn->error;
 }
